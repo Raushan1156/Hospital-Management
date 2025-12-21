bb8baa05bc7830f825e35faf0f36708471e3a3ce
Raushan1156 <iamraushankumar56@gmail.com>
2025-12-22 00:34:46 +0530

    Added servers details
    Added local and production url

--- src/main/java/com/HospitalManagement/HospitalManagementApplication.java
+++ src/main/java/com/HospitalManagement/HospitalManagementApplication.java
@@ -2,6 +2,7 @@ package com.HospitalManagement;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +13,11 @@ import java.util.TimeZone;
 				title = "Hospital Management API",
 				version = "1.0.0",
 				description = "API documentation for Hospital Management system"
-		)
+		),
+		servers = {
+				@Server(url = "http://localhost:9001", description = "Local"),
+				@Server(url = "https://api.mycompany.com", description = "Prod")
+		}
 )
 
 @SpringBootApplication
