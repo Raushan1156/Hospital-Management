154663d386be96daae22df4d058248f3f7b8e252
Raushan1156 <iamraushankumar56@gmail.com>
2025-12-22 00:21:40 +0530

    Re-configure the openApi doc
    Used @OpenAPIDefinition and @Info

--- src/main/java/com/HospitalManagement/HospitalManagementApplication.java
+++ src/main/java/com/HospitalManagement/HospitalManagementApplication.java
@@ -1,9 +1,19 @@
 package com.HospitalManagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.TimeZone;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Hospital Management API",
+				version = "1.0.0",
+				description = "API documentation for Hospital Management system"
+		)
+)
 
 @SpringBootApplication
 public class HospitalManagementApplication {
